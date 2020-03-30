e1293425a9a63403dddcbb8a7e4273cd757417c2
Yoshi <yoshijava@gmail.com>
2020-03-30 18:27:13 +0800

    Reduce QuoteII query period to 15s

--- src/main/java/tbroker/QuoteII.java
+++ src/main/java/tbroker/QuoteII.java
@@ -30,7 +30,7 @@ import java.util.concurrent.*;
 import org.json.*;
 
 public class QuoteII extends RPCClient implements Quote {
-    static final long PERIOD = 30 * 1000L;
+    static final long PERIOD = 15 * 1000L;
 
     static final int POOL = 3;
 
